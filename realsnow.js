19d50d3a8625f1bea37fb72acec39a397d983623
lybell <32293736+lybell-art@users.noreply.github.com>
2023-09-25 15:05:40 +0900

    Remake to modern javascript

--- realsnow.js
+++ realsnow.js
@@ -1,42 +1,46 @@
-var max_n=1000;
-var n=0;
-var s=[];
+/**
+ * 2023 remake ver.(using ES6)
+ */
+
+const MAX_SNOW_NUMBER = 1000;
+const snows = Array.from({length:1000}, ()=>new Snow());
+let indice = 0;
+
+class Snow
+{
+	x = 0;
+	y = 0;
+	size = 10;
+	velocity = 5;
+	active = false;
+	create()
+	{
+		this.x = random(width);
+		this.y = 0;
+		this.size = random(5,15);
+		this.velocity = random(3,8);
+		this.active = true;
+	}
+	drop()
+	{
+		if(!this.active) return;
+		ellipse(this.x,this.y,this.size,this.size);
+		this.y+=this.velocity;
+		if(this.y > windowHeight) this.active = false;
+	}
+}
+
 function setup()
 {
-  createCanvas(windowWidth,windowHeight);
-  background("#cccccc");
-  noStroke();
-  fill(255);
-  for(var i=0;i<max_n;i++) s.push(new snow());
+	createCanvas(windowWidth, windowHeight);
+	background("#cccccc");
+	noStroke();
+	fill(255);
 }
+
 function draw()
 {
-  background("#cccccc");
-  s[n++%max_n].create();
-  for(var i=0;i<max_n;i++)
-  {
-    s[i].drop();
-  }
-}
-function snow()
-{
-	var x,y,size,vel;
-	var ison=false;
-	this.create=function()
-	{
-		this.x=random(width);
-    this.y=0;
-    this.size=random(5,15);
-    this.vel=random(3,8);
-    this.ison=true;
-	}
-	this.drop=function()
-  {
-    if(this.ison)
-    {
-      ellipse(this.x,this.y,this.size,this.size);
-      this.y+=this.vel;
-      if(this.y>height) this.ison=false;
-    }
-  }
+	background("#cccccc");
+	snows[indice++ % MAX_SNOW_NUMBER].create();
+	snows.forEach( snow=>snow.drop() );
 }
